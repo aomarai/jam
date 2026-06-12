32ca09b449ff5963d2015795eb64a4fb3ed85821
Ash Omaraie <ashomaraie@icloud.com>
2026-06-12 02:07:58 +0000

    Adding config retrieval logic

---
+++ internal/config/config.go
@@ -0,0 +1,56 @@
+package config
+
+import (
+	"log/slog"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	TwitchOAuthToken    string
+	TwitchBotUsername   string
+	TwitchTargetChannel string
+	MMarkovStateSize    int
+	PostInterval        time.Duration
+	SaveInterval        time.Duration
+	ModelPath           string
+	SeedPath            string
+}
+
+// getEnv returns the value of an environment variable or a fallback if it is not set.
+func getEnv(key, fallback string) string {
+	if value := os.Getenv(key); value != "" {
+		return value
+	}
+	return fallback
+}
+
+// Load retrieves the configuration from environment variables and loads it into the application.
+func Load() (*Config, error) {
+	slog.Info("Loading configuration...")
+	stateSize, err := strconv.Atoi(getEnv("MARKOV_STATE_SIZE", "3"))
+	if err != nil {
+		return nil, err
+	}
+
+	postInterval, err := time.ParseDuration(getEnv("POST_INTERVAL", "1h"))
+	if err != nil {
+		return nil, err
+	}
+
+	saveInterval, err := time.ParseDuration(getEnv("SAVE_INTERVAL", "24h"))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		TwitchBotUsername:   getEnv("TWITCH_BOT_USERNAME", ""),
+		TwitchTargetChannel: getEnv("TWITCH_TARGET_CHANNEL", ""),
+		MMarkovStateSize:    stateSize,
+		PostInterval:        postInterval,
+		SaveInterval:        saveInterval,
+		ModelPath:           getEnv("MODEL_PATH", "model.json"),
+		SeedPath:            getEnv("SEED_PATH", "seed.txt"),
+	}, nil
+}
